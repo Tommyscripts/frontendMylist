2cf72fe0211965f7d8d969466ad2fbca6c94bc90
Tommyscripts <dark.angel.alhue@gmail.com>
2023-03-21 11:17:01 +0000

    Working in home view

--- Mylist/src/views/HomeView.vue
+++ Mylist/src/views/HomeView.vue
@@ -1,30 +1,33 @@
-<template >
+<template>
   <div class="back" id="colores">
-Bienvenido/a
-<v-divider></v-divider>
-Aqui podrás crear varias listas de compra y productos, desde los productos que tenemos en casa, a la lista de la compra, listas personalizadas. Todos los productos están separado por categorías. Para ello necesitarás crear una cuenta.
-<v-divider></v-divider>
-<v-btn rounded color="#375B83">
-  <a id="colores">Crear una cuenta ahora</a>
-  
-</v-btn>
-</div>
+    Bienvenido/a
+    <v-divider></v-divider>
+    Aqui podrás crear varias listas de compra y productos, desde los productos
+    que tenemos en casa, a la lista de la compra, listas personalizadas. Todos
+    los productos están separado por categorías. Para ello necesitarás crear una
+    cuenta.
+    <v-divider></v-divider>
+    <v-btn rounded color="#375B83">
+      <a id="colores">Crear una cuenta ahora</a>
+    </v-btn>
+    <v-divider></v-divider>
+    <v-btn rounded color="#375B83">
+      <a id="colores"><v-icon>mdi-arrow-left</v-icon> Back</a>
+    </v-btn>
+  </div>
 </template>
 
 <script>
-
-
 // Components
 export default {
   name: "HomeView",
-
 };
 </script>
 <style>
-#colores{
-  color: #4BA435;
+#colores {
+  color: #4ba435;
 }
-.back{
-  background-color: #363B6A;
+.back {
+  background-color: #363b6a;
 }
 </style>
