4912e1a71968096e89c4a08da20ee4feb3ef44e4
Tommyscripts <dark.angel.alhue@gmail.com>
2023-04-10 09:06:32 +0100

    update: Llist service prodcts service admin.vue

---
+++ Mylist/src/services/list.js
@@ -0,0 +1,60 @@
+import axios from 'axios'
+import { useAuthStore } from '../stores/store'
+
+
+const API = axios.create({
+  baseURL: 'http://localhost:3000/api',
+  headers: {token: localStorage.getItem('token')}
+})
+
+async function getList() {
+    try {
+      const response = await API.get("/list");
+      return response.data;
+    } catch (error) {
+      return { error: error.message };
+    }
+  }
+  async function createList(newList) {
+    const store = useAuthStore();
+    try {
+      const response = await API.post("/list", newList, {
+        headers: {
+          token: store.token,
+        },
+      });
+      return response.data;
+    } catch (error) {
+      return { error: error.message };
+    }
+  }
+  async function updateList (newData) {
+    const store = useAuthStore()
+    try {
+      const response = await API.put('/list', newData, {
+        headers: {
+          token: store.token,
+        }
+      })
+      return response.data
+    } catch (error) {
+      return { error: error.message }
+    }
+  }
+
+  async function deleteList(id) {
+    try {
+      const response = await API.get(`/list/${id}`);
+      return response.data;
+    } catch (error) {
+      return { error: error.message };
+    }
+  }
+
+  export default{
+    getList,
+    createList,
+    updateList,
+    deleteList
+
+  }
