d7eb899aa481707ffe15e9fcbdfc89627cc76d02
Tommyscripts <dark.angel.alhue@gmail.com>
2023-04-10 09:20:56 +0100

    Update: Crear producto component

---
+++ Mylist/src/components/CrearProducto.vue
@@ -0,0 +1,61 @@
+<template>
+    <v-col cols="12" sm="7" class="mt-10 mx-auto">
+      <v-text-field
+        label="Img"
+        :rules="rules"
+        hide-details="auto"
+        v-model="newProducts.Img"
+      ></v-text-field>
+      <v-text-field
+        label="Name"
+        :rules="rules"
+        v-model="newProducts.Name"
+      ></v-text-field>
+      <v-select
+        label="Categorias"
+        :items="categorias"
+        v-model="newProducts.categorias"
+      ></v-select>
+      <v-btn
+        elevation="2"
+        color="#375B83"
+        class="amber--text text--darken-1"
+        rounded
+        dark
+        @click.prevent="addNewProduct"
+        @keydown.enter.prevent="addNewProduct"
+      >
+        Añadir</v-btn
+      >
+    </v-col>
+  </template>
+
+<script>
+import products from "../services/productos";
+
+export default {
+  data() {
+    return {
+      newProducts: {
+        img: "",
+        name: "",
+        categorias: ["Carnes", "Pescados", "Congelados", "Arroces", "Lacteos y huevos", "Pasta", "Verduras", "Frutas", "Embutidos", "Panaderia", "Frutos secos", "Salsas", "Encurtidos", "Café y azúcar", "Bollería", "Limpieza"],
+      },
+      rules: [
+        (value) => !!value || "Required.",
+        (value) => (value && value.length >= 3) || "Min 3 characters",
+      ],
+    };
+  },
+  methods: {
+    async addNewProduct() {
+      const response = await products.addProduct(this.newProducts);
+      if (response === "error") {
+        alert("Error creando el producto");
+      } else {
+        this.$router.push({ name: "products" });
+      }
+    },
+  },
+};
+</script>
