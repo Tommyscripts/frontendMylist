e9523c7fe0a80e4678b82cf37260aac38bf31e0e
Tommyscripts <javier.cabrera.escoz@gmail.com>
2023-10-26 12:18:35 +0100

    he dado una pequeña separacion a los botones de profileuser, dandole mejor vision

--- Mylist/src/components/profileuser.vue
+++ Mylist/src/components/profileuser.vue
@@ -1,10 +1,10 @@
 <template>
   <v-container>
     <v-row class="pa-5">
-      <v-col cols="12"   class="mx-auto">
+      <v-col cols="12" class="mx-auto">
         <v-row>
           <v-spacer></v-spacer>
-          <v-col cols="12"   class="mx-auto" >
+          <v-col cols="12" class="mx-auto">
             <v-sheet class="colores"> Bienvenido/a: {{ users.username }} </v-sheet>
           </v-col>
         </v-row>
@@ -12,18 +12,18 @@
       </v-col>
     </v-row>
     <v-col>
-      <v-row >
-        <v-btn class="ml-4" rounded color="#375B83" @click="retroceder()">
+      <v-row>
+        <v-btn class="ml-4 mt-2" rounded color="#375B83" @click="retroceder">
           <a id="colores"><v-icon>mdi-arrow-left</v-icon> Back</a>
         </v-btn>
-        <v-btn class="ml-4" rounded color="#375B83" @click="borrarCuenta()">
-          <a id="colores"
-            ><v-icon>mdi-close-octagon-outline</v-icon> Eliminar Cuenta</a>
+        <v-btn class="ml-4 mt-2" rounded color="#375B83" @click="borrarCuenta">
+          <a id="colores"><v-icon>mdi-close-octagon-outline</v-icon> Eliminar Cuenta</a>
         </v-btn>
       </v-row>
     </v-col>
   </v-container>
 </template>
+
 <script>
 import { RouterLink } from "vue-router";
 import api from "../services/api.js";
@@ -55,16 +55,16 @@ export default {
 </script>
 
 <style scoped>
-.colores{
+.colores {
   background-color: #363b6a;
   color: white;
   font-family: "Montserrat", sans-serif;
-  font-size: calc(16px+1vw);
+  font-size: calc(16px + 1vw);
   font-weight: bold;
 }
 #text {
   font-family: "Montserrat", sans-serif;
-  font-size: calc(16px+1vw);
+  font-size: calc(16px + 1vw);
   font-weight: bold;
 }
 .btn {
