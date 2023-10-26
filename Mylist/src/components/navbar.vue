fdbf0cfb587200e4e3ad4d0ba94063d2bfc9e4ea
Tommyscripts <javier.cabrera.escoz@gmail.com>
2023-10-26 09:45:47 +0100

    Navbar admin bug fixed

--- Mylist/src/components/navbar.vue
+++ Mylist/src/components/navbar.vue
@@ -24,35 +24,34 @@
               Home
             </v-list-item>
 
-            <v-list-item rounded dark :to="{name: 'soporte'}"> 
-              Soporte 
+            <v-list-item rounded dark :to="{ name: 'soporte' }">
+              Soporte
             </v-list-item>
 
-            <div v-if="!store.isLoggedIn">
-              <v-list-item rounded dark :to="{ name: 'login' }">
-                Login
-              </v-list-item>
-            </div>
-            <div v-if="store.isLoggedIn && role === 'admin' ">
-              <v-list-item rounded dark :to="{ name: 'admin' }">
-                Admin Site
-              </v-list-item>
-            </div>
-            <div v-if="store.isLoggedIn">
-              <v-list-item rounded dark :to="{ name: 'profile' }">
-                Profile
-              </v-list-item>
-              <v-divider></v-divider>
-              <v-list-item rounded dark @click="logout()">
-                Log Out
-              </v-list-item>
-            </div>
+            <v-list-item rounded dark :to="{ name: 'login' }" v-if="!store.isLoggedIn">
+              Login
+            </v-list-item>
+
+            <v-list-item rounded dark :to="{ name: 'admin' }" v-if="store.isLoggedIn && role === 'admin'">
+              Admin Site
+            </v-list-item>
+
+            <v-list-item rounded dark :to="{ name: 'profile' }" v-if="store.isLoggedIn">
+              Profile
+            </v-list-item>
+
+            <v-divider v-if="store.isLoggedIn"></v-divider>
+
+            <v-list-item rounded dark @click="logout" v-if="store.isLoggedIn">
+              Log Out
+            </v-list-item>
           </v-list>
         </v-navigation-drawer>
       </div>
     </v-col>
   </v-row>
 </template>
+
 <script>
 import { useAuthStore } from "../stores/store";
 import api from "../services/api";
@@ -63,35 +62,46 @@ export default {
   data: () => ({
     drawer: false,
     group: null,
-    store: useAuthStore(),
     role: "",
-    auth: "admin",
   }),
+
+  computed: {
+    store() {
+      return useAuthStore();
+    },
+  },
+
   methods: {
     logout() {
       this.store.logout();
       this.$router.push({ name: "home" });
     },
+    async getUserData() {
+      try {
+        this.isLoading = true;
+        const userData = await api.getUser();
+        this.role = userData.role; // Asegúrate de que "userData.role" sea la propiedad correcta que contiene el rol
+        this.isLoading = false;
+      } catch (error) {
+        this.isLoading = false;
+        // Manejo de errores, por ejemplo, mostrar un mensaje de error
+        console.error('Error al obtener los datos del usuario:', error);
+      }
+    },
   },
-  async getUserData() {
-  try {
-    const userData = await api.getUser();
-    this.role = userData.role;
-    this.isLoading = false;
-    window.location.reload(); // Recargar la página después de iniciar sesión
-  } catch (error) {
-    // Manejo de errores, por ejemplo, mostrar un mensaje de error
-    console.error('Error al obtener los datos del usuario:', error);
-  }
-},
 
   watch: {
     group() {
       this.drawer = false;
     },
   },
+
+  created() {
+    this.getUserData(); // Llama a getUserData al crear el componente
+  },
 };
 </script>
+
 <style>
 #colores {
   color: #001d3d;
@@ -99,5 +109,4 @@ export default {
 #ancho {
   max-width: 90%;
 }
-
 </style>
